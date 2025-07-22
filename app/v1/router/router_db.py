da4d31c93c69a7416b8d18d2595b364f7c11ac93
Juan Sanchez <juanjosesanchezgil@gmail.com>
2025-07-22 08:25:47 +0200

    añadioendo cosas a db

--- app/v1/router/router_db.py
+++ app/v1/router/router_db.py
@@ -4,4 +4,13 @@ from sqlmodel import Field, Session, SQLModel, create_engine, select
 
 class Task(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
-    
+    title: str = Field(index=True)
+    description: str | None = Field(default=None, index=True)
+    complete: bool = Field(index=True)
+
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+
+connect
